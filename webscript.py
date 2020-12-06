6d572de73b6a32266ba9003b43220ca4d7e57d8d
RATNADEEP YSVS <68638971+RatnadeepYSVS@users.noreply.github.com>
2020-12-06 13:11:57 +0530

    Update webscript.py

--- webscript.py
+++ webscript.py
@@ -1,47 +1,43 @@
 from datetime import datetime
 from pynput.keyboard import Key,Controller
-from pynput.mouse import Controller as Ctr,Button
-import time,os
-def whatssend(person,mess):
-    os.system(r"C:\Users\ratna\Desktop\whatsapp.lnk")
+import time,webbrowser
+def whatssend(date,month_num,person,mess):
+    webbrowser.open('https://web.whatsapp.com/')
     time.sleep(20)
-    mouse=Ctr()
     keyboard=Controller()
-    mouse.position=(200, 120)
-    mouse.click(Button.left,1)
+    keyboard.press(Key.tab)
+    time.sleep(3)
     for i in person:
         keyboard.press(i)
     time.sleep(8)
-    keyboard.press(Key.down)
-    time.sleep(2)
-    mouse.position=(580,650)
-    mouse.click(Button.left,1)
-    time.sleep(2)
+    keyboard.press(Key.tab)
+    time.sleep(5)
+    for i in range(2):
+        keyboard.press(Key.tab)
+    time.sleep(5)
     while True:
-        if datetime.now().hour==00 and datetime.now().minute==00:
+        if (datetime.now().day==date and datetime.now().month==month_num) and (datetime.now().hour==00 and datetime.now().minute==00):
             for i in mess:
                 keyboard.press(i)
-            time.sleep(2)
+            time.sleep(5)
             keyboard.press(Key.enter)
             time.sleep(10)
             break
 def normal_mes(person,msg):
-    os.system(r"C:\Users\ratna\Desktop\whatsapp.lnk")
+    webbrowser.open('https://web.whatsapp.com/')
     time.sleep(20)
-    mouse = Ctr()
-    keyboard = Controller()
-    mouse.position = (200, 120)
-    mouse.click(Button.left, 1)
+    keyboard=Controller()
+    keyboard.press(Key.tab)
+    time.sleep(3)
     for i in person:
         keyboard.press(i)
-    time.sleep(8)
-    keyboard.press(Key.down)
-    time.sleep(2)
-    mouse.position = (580, 650)
-    mouse.click(Button.left, 1)
-    time.sleep(2)
+    time.sleep(5)
+    keyboard.press(Key.tab)
+    time.sleep(5)
+    for i in range(2):
+        keyboard.press(Key.tab)
+    time.sleep(5)
     for i in msg:
         keyboard.press(i)
-    time.sleep(2)
+    time.sleep(5)
     keyboard.press(Key.enter)
-    time.sleep(10)
